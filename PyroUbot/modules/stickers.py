87b84c55aa304d0479de128929e5832697078ef2
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-07-05 10:26:25 +0700

    UserBot

--- PyroUbot/modules/stickers.py
+++ PyroUbot/modules/stickers.py
@@ -16,7 +16,7 @@ nomber_stiker = "0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 2
 
 from PyroUbot import *
 
-__MODULE__ = "Sticker"
+__MODULE__ = "ꜱᴛɪᴄᴋᴇʀ"
 __HELP__ = """
 <blockquote>Bantuan Untuk Sticker
 
