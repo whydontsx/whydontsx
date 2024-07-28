168d8e06483fe8d6a125eec7f4bd16095ace0b73
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-07-28 19:25:04 +0700

    Update ai-image.py

--- PyroUbot/modules/ai-image.py
+++ PyroUbot/modules/ai-image.py
@@ -12,7 +12,46 @@ __HELP__ = """
    <i>penjelasan:</b> untuk stalking instagram menggunakan username</i></blockquote>
 """
 
-@PY.UBOT("ai-image")
+@PY.UBOT("aiimage")
+async def stalkig(client, message):
+    ggl = await EMO.GAGAL(client)
+    sks = await EMO.BERHASIL(client)
+    prs = await EMO.PROSES(client)
+    
+    jalan = await message.reply(f"{prs} Processing...")
+    
+    if len(message.command) != 2:
+        return await jalan.edit(f"{ggl} Please use the command `stalkig` followed by the Instagram username.")
+    
+    username = message.command[1]
+    chat_id = message.chat.id
+    url = f"https://widipe.com/bingimg?text={username}"
+    
+    try:
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            hasil = data['result']
+            photoUrl = hasil['result']
+            caption = f"""
+<b><emoji id=5841235769728962577>⭐</emoji></b>
+"""
+            photo_path = wget.download(photoUrl)
+            await client.send_photo(chat_id, caption=caption, photo=photo_path)
+            if os.path.exists(photo_path):
+                os.remove(photo_path)
+            
+            await jalan.delete()
+        else:
+            await jalan.edit(f"{ggl} No 'result' key found in the response.")
+    
+    except requests.exceptions.RequestException as e:
+        await jalan.edit(f"{ggl} Request failed: {e}")
+    
+    except Exception as e:
+        await jalan.edit(f"{ggl} An error occurred: {e}")
+      
+#@PY.UBOT("ai-image")
 async def stalkig(client, message):
     ggl = await EMO.GAGAL(client)
     sks = await EMO.BERHASIL(client)
