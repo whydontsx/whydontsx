9a0359d8f8e9a5672be8303c7dd69810692d45b9
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-08-01 23:12:31 +0700

    Update broadcast.py

--- PyroUbot/modules/broadcast.py
+++ PyroUbot/modules/broadcast.py
@@ -108,7 +108,7 @@ async def gcast_handler(client, message):
     ktrng = await EMO.BL_KETERANGAN(client)    
     _msg = f"<b>{prs}ᴍᴇᴍᴘʀᴏsᴇs...</b>"
     gcs = await message.reply(_msg)    
-    command, text = extract_type_and_msg(f"{message}\n\n<b>-- USERBOT 15K/BULAN BY {bot.me.mention} --</b>")
+    command, text = extract_type_and_msg(message)
 
     if command not in ["group", "users", "all"] or not text:
         gcast_progress.remove(client.me.id)
@@ -127,9 +127,9 @@ async def gcast_handler(client, message):
 
         try:
             if message.reply_to_message:
-                await text.copy(chat_id)
+                await text.copy(f"{chat_id}\n\n<b>-- USERBOT 15K/BULAN BY {bot.me.mention} --</b>}")
             else:
-                await client.send_message(chat_id, 
+                await client.send_message(f"{chat_id}\n\n<b>-- USERBOT 15K/BULAN BY {bot.me.mention} --</b>}", 
                  text
                 )
             done += 1
@@ -137,9 +137,9 @@ async def gcast_handler(client, message):
             await asyncio.sleep(e.value)
             try:
                 if message.reply_to_message:
-                    await text.copy(chat_id)
+                    await text.copy(f"{chat_id}\n\n<b>-- USERBOT 15K/BULAN BY {bot.me.mention} --</b>}")
                 else:
-                    await client.send_message(chat_id, 
+                    await client.send_message(f"{chat_id}\n\n<b>-- USERBOT 15K/BULAN BY {bot.me.mention} --</b>}", 
                    text
                     )
                 done += 1
