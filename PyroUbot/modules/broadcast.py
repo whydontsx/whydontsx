036f12e903eb7cdb55585fb5962d20185d622792
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-08-02 21:21:33 +0700

    Update broadcast.py

--- PyroUbot/modules/broadcast.py
+++ PyroUbot/modules/broadcast.py
@@ -98,65 +98,65 @@ gcast_progress = []
 @PY.UBOT("bc|gikes")
 @PY.TOP_CMD
 async def gcast_handler(client, message):
-    global gcast_progress
-    gcast_progress.append(client.me.id)
-    
-    prs = await EMO.PROSES(client)
-    sks = await EMO.BERHASIL(client)
-    ggl = await EMO.GAGAL(client)
-    bcs = await EMO.BROADCAST(client)
-    ktrng = await EMO.BL_KETERANGAN(client)    
-    _msg = f"<b>{prs}ᴍᴇᴍᴘʀᴏsᴇs...</b>"
-    gcs = await message.reply(_msg)    
-    command, text = extract_type_and_msg(message)
+    global gcast_progress
+    gcast_progress.append(client.me.id)
+    
+    prs = await EMO.PROSES(client)
+    sks = await EMO.BERHASIL(client)
+    ggl = await EMO.GAGAL(client)
+    bcs = await EMO.BROADCAST(client)
+    ktrng = await EMO.BL_KETERANGAN(client)    
+    _msg = f"<b>{prs}ᴍᴇᴍᴘʀᴏsᴇs...</b>"
+    gcs = await message.reply(_msg)    
+    command, text = extract_type_and_msg(message)
 
-    if command not in ["group", "users", "all"] or not text:
-        gcast_progress.remove(client.me.id)
-        return await gcs.edit(f"{ggl}{message.text.split()[0]} ᴛʏᴘᴇ [ʀᴇᴘʟʏ]")
-    chats = await get_data_id(client, command)
-    blacklist = await get_list_from_vars(client.me.id, "BL_ID")
+    if command not in ["group", "users", "all"] or not text:
+        gcast_progress.remove(client.me.id)
+        return await gcs.edit(f"{ggl}{message.text.split()[0]} ᴛʏᴘᴇ [ʀᴇᴘʟʏ]")
+    chats = await get_data_id(client, command)
+    blacklist = await get_list_from_vars(client.me.id, "BL_ID")
 
-    done = 0
-    failed = 0
-    for chat_id in chats:
-        if client.me.id not in gcast_progress:
-            await gcs.edit(f"<blockquote><b>ᴘʀᴏsᴇs ɢᴄᴀsᴛ ʙᴇʀʜᴀsɪʟ ᴅɪ ʙᴀᴛᴀʟᴋᴀɴ !</b> {sks}</blockquote>")
-            return
-        if chat_id in blacklist or chat_id in BLACKLIST_CHAT:
-            continue
+    done = 0
+    failed = 0
+    for chat_id in chats:
+        if client.me.id not in gcast_progress:
+            await gcs.edit(f"<blockquote><b>ᴘʀᴏsᴇs ɢᴄᴀsᴛ ʙᴇʀʜᴀsɪʟ ᴅɪ ʙᴀᴛᴀʟᴋᴀɴ !</b> {sks}</blockquote>")
+            return
+        if chat_id in blacklist or chat_id in BLACKLIST_CHAT:
+            continue
 
-        try:
-            if message.reply_to_message:
-                await text.copy(chat_id)
-            else:
-                await client.send_message(chat_id, 
-                 f"{text}\n\nBY : USU"
-                )
-            done += 1
-        except FloodWait as e:
-            await asyncio.sleep(e.value)
-            try:
-                if message.reply_to_message:
-                    await text.copy(chat_id)
-                else:
-                    await client.send_message(chat_id, 
-                 f"{text}\n\nBY : USU"
-                    )
-                done += 1
-            except (Exception, ChannelPrivate):
-                failed += 1
-        except (Exception, ChannelPrivate):
-            failed += 1
+        try:
+            if message.reply_to_message:
+                await text.copy(chat_id)
+            else:
+                await client.send_message(chat_id, 
+                 f"{text}\n\nBY : USU"
+                )
+            done += 1
+        except FloodWait as e:
+            await asyncio.sleep(e.value)
+            try:
+                if message.reply_to_message:
+                    await text.copy(chat_id)
+                else:
+                    await client.send_message(chat_id, 
+                   f"{text}\n\nBY : USU"
+                    )
+                done += 1
+            except (Exception, ChannelPrivate):
+                failed += 1
+        except (Exception, ChannelPrivate):
+            failed += 1
 
-    gcast_progress.remove(client.me.id)
-    await gcs.delete()
-    _gcs = f"""
+    gcast_progress.remove(client.me.id)
+    await gcs.delete()
+    _gcs = f"""
 <blockquote><b>{bcs}ʙʀᴏᴀᴅᴄᴀsᴛ ᴛᴇʀᴋɪʀɪᴍ</b></blockquote>
 <blockquote><b>{sks}ʙᴇʀʜᴀsɪʟ : {done} ᴄʜᴀᴛ</b>
 <b>{ggl}ɢᴀɢᴀʟ : {failed} ᴄʜᴀᴛ</b>
 <b>{ktrng}ᴛʏᴘᴇ :</b> <code>{command}</code></blockquote>
 """
-    return await message.reply(_gcs)
+    return await message.reply(_gcs)
     
 
 @PY.UBOT("stopg")
