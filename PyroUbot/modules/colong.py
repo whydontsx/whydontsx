f0258eec23f6bbf1b2250e44f5f0436131cd850c
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-08-27 20:17:20 +0700

    UserBot

---
+++ PyroUbot/modules/colong.py
@@ -0,0 +1,7 @@
+__MODULE__ = "ᴄᴏʟᴏɴɢ"
+__HELP__ = """
+<blockquote><b>Bantuan Untuk Colong
+
+perintah : <code>{0}colong</code>
+    untuk mengambil media/vidio yang 1x lihat</b></blockquote>
+"""
---
+++ PyroUbot/modules/sg.py
@@ -0,0 +1,7 @@
+__MODULE__ = "ꜱᴀɴɢᴍᴀᴛᴀ"
+__HELP__ = """
+<blockquote><b>Bantuan untuk sangmata
+
+perintah : <code>{0}sg</code> [reply/user]
+    memeriksa histori name pengguna telegram</b></blockquote>
+"""
