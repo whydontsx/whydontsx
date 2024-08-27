14755fb0c01429bc319f24c978787575cdab2cd8
WannFyy <165775366+wndrzzka@users.noreply.github.com>
2024-08-27 19:55:18 +0700

    UserBot

--- PyroUbot/modules/tiktok.py
+++ PyroUbot/modules/tiktok.py
@@ -1,125 +1,59 @@
-import bs4
+import asyncio
 import requests
-
+import os
+from pyrogram.raw.functions.messages import DeleteHistory
+import aiohttp
 from PyroUbot import *
 
 __MODULE__ = "ᴛɪᴋᴛᴏᴋ"
 __HELP__ = """
 <blockquote> <b>Bantuan Untuk Tiktok</b>
 
-• <b>Perintah</b> : <code>{0}tt</code> <b>[link nya]</b>
-• <b>Penjelasan : Download Vt No Vm</b></blockquote>
+• <b>Perintah</b> : <code>{0}tt</code> -v atau -m <b>[link nya]</b>
+• <b>Penjelasan : Download Vt No Vm, -v untuk video -m untuk musik.</b></blockquote>
 
 """
 
-
-class TikTokDownloaderAPI:
-    def __init__(self):
-        pass
-
-    def ttscraper(self, url, output_name):
-        ses = requests.Session()
-        i = ses.post("https://ytpp3.com/ttscraper/parse", data={"url": url})
-        if '"message":"success"' in i.text:
-            load = i.json()
-            url_download = load["data"]["nwm_video_url"]
-            get_content = requests.get(url_download)
-            with open(output_name, "wb") as fd:
-                fd.write(get_content.content)
-            return True
-        else:
-            return False
-
-    def downloader(self, url, output_name):
-        """
-        url: tiktok video url
-        output_name: output video (.mp4). Example : video.mp4
-        """
-        ses = requests.Session()
-        server_url = "https://musicaldown.com/"
-        headers = {
-            "Host": "musicaldown.com",
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:103.0) Gecko/20100101 Firefox/103.0",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-            "Accept-Language": "en-US,en;q=0.5",
-            "DNT": "1",
-            "Upgrade-Insecure-Requests": "1",
-            "Sec-Fetch-Dest": "document",
-            "Sec-Fetch-Mode": "navigate",
-            "Sec-Fetch-Site": "none",
-            "Sec-Fetch-User": "?1",
-            "TE": "trailers",
-        }
-        ses.headers.update(headers)
-        req = ses.get(server_url)
-        data = {}
-        parse = bs4.BeautifulSoup(req.text, "html.parser")
-        get_all_input = parse.findAll("input")
-
-        for i in get_all_input:
-            if i.get("id") == "link_url":
-                data[i.get("name")] = url
+async def download_file(url, path):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                with open(path, 'wb') as f:
+                    f.write(await response.read())
             else:
-                data[i.get("name")] = i.get("value")
+                print(f"Failed to download file: {response.status}")
 
-        post_url = server_url + "id/download"
-        req_post = ses.post(post_url, data=data, allow_redirects=True)
-        if (
-            req_post.status_code == 302
-            or "This video is currently not available" in req_post.text
-            or "Video is private or removed!" in req_post.text
-        ):
-            print("- video private or remove")
-            return "private/remove"
-        elif "Submitted Url is Invalid, Try Again" in req_post.text:
-            print("- url is invalid")
-            return "url-invalid"
-        get_all_blank = bs4.BeautifulSoup(req_post.text, "html.parser").findAll(
-            "a", attrs={"target": "_blank"}
-        )
-
-        download_link = get_all_blank[0].get("href")
-        get_content = requests.get(download_link)
-
-        with open(output_name, "wb") as fd:
-            fd.write(get_content.content)
-        return True
-
-
-# tiktok downloader
-@PY.UBOT("tt")
-async def tiktok(client, message):
-    if len(message.command) < 2:
-        text = (
-            "<blockquote>Pengunaan : .tt https://vt.tiktok.com/ZSN5V7BFX/</blockquote>"
-        )
-        return await message.reply_text(text, disable_web_page_preview=True)
-
-    val = TikTokDownloaderAPI()
-    value = message.text.split(None, 1)[1]
-
-    if "tiktok.com" in value and "https://" in value:
-        message_wait = await message.reply_text("<blockquote>🔍 <b>Memprosess...</b></blockquote>")
-
-        video = val.downloader(url=value, output_name="video.mp4")
-        video_id = open("video.mp4", "rb")
-
-        if video:
-            await client.send_video(
-                message.chat.id, video_id, caption="<blockquote>Berikut Vidio Yang Anda Minta</blockquote>"
-            )
+async def downloader_tiktok(client, message, perintah,tujuan):
+    url = f"https://widipe.com/download/tiktokdl?url={tujuan}"
+    headers = {"accept": "application/json"}
+    res = requests.get(url, headers=headers)
+    if res.status_code == 200:
+        data = res.json()
+        if data['status'] and data['code'] == 200:
+            if perintah == "-v":
+                video_url = data['result']['video']
+                video_path = "video.mp4"
+                await download_file(video_url, video_path)
+                await client.send_video(message.chat.id, video_path, caption=f"Download by: {client.me.mention}")
+                os.remove(video_path)
+            elif perintah == "-m":
+                music_url = data['result']['music']
+                music_path = "music.mp3"
+                await download_file(music_url, music_path)
+                await client.send_audio(message.chat.id, music_path, caption=f"<b>Download by: {client.me.mention}</b>")
+                os.remove(music_path)
+            else:
+                return await message.reply("Gunakan format -v atau -m.")
         else:
-            await message_wait.edit(
-                "<blockquote>Maaf, saya tidak dapat mendapatkan informasi tentang file ini.\nCoba lagi nanti atau kirim tautan lain.</blockquote>"
-            )
-
-        await message_wait.delete()
+            print("Failed to fetch download URLs.")
     else:
-        text = (
-            "<blockquote>Pengunaan : .tt https://vt.tiktok.com/ZSN5V7BFX/</blockquote>"
-        )
-        return await client.send_message(
-            message.chat.id, text, disable_web_page_preview=True
-        )
+        print(f"Request failed with status code {res.status_code}")
 
-    await message.delete()
+@PY.UBOT("tt")
+async def _(client, message):
+    if len(message.command) < 3:
+        return await message.reply(
+            f"<code>{message.text.split()[0]}</code> use -v for video or use -m for audio")
+    command, isi = message.command[:2]
+    link = " ".join(message.command[2:])
+    return await downloader_tiktok(client, message, isi, link)
