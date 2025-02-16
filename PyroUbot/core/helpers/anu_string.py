0291c10aad8129150ee9ab6470e85ee4954f353f
baverlytzy <baverlytzy@gmail.com>
2025-02-16 12:32:58 +0700

    Update anu_string.py

--- PyroUbot/core/helpers/anu_string.py
+++ PyroUbot/core/helpers/anu_string.py
@@ -3,19 +3,19 @@ from PyroUbot import *
 class STR:
     async def PONG(client):
         str_pong = await get_vars(client.me.id, "STRING_PONG")
-        string_pong = str_pong if str_pong else "± ᴘᴏɴɢ"
+        string_pong = str_pong if str_pong else "ᴘᴏɴɢ"
         result = f"{string_pong}"
         return result
 
     async def OWNER(client):
         str_pong = await get_vars(client.me.id, "STRING_OWNER")
-        string_pong = str_pong if str_pong else "± ᴏᴡɴᴇʀ"
+        string_pong = str_pong if str_pong else "ᴏᴡɴᴇʀ"
         result = f"{string_pong}"
         return result
 
     async def UBOT(client):
         str_pong = await get_vars(client.me.id, "STRING_UBOT")
-        string_pong = str_pong if str_pong else "± ᴜʙᴏᴛ"
+        string_pong = str_pong if str_pong else "ᴜʙᴏᴛ"
         result = f"{string_pong}"
         return result
 
